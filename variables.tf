2ec8cb1320f296f0db582cce266267776ab17ebb
sergiogordon <13407640+sergiogordon@users.noreply.github.com>
2024-08-07 10:26:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-# variables.tf in modules-bp module
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
@@ -17,14 +16,17 @@ variable "public_subnet_cidrs" {
 variable "enable_dns_support" {
   description = "Enable DNS support in the VPC"
   type        = bool
+  default     = true
 }
 
 variable "enable_dns_hostnames" {
   description = "Enable DNS hostnames in the VPC"
   type        = bool
+  default     = true
 }
 
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
+  default     = {}
 }
