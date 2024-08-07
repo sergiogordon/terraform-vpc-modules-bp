46240a2cf676a1ef037078e0681651f18380a291
sergiogordon <13407640+sergiogordon@users.noreply.github.com>
2024-08-07 08:08:10 -0500

    Update vpcmain.tf

--- vpcmain.tf
+++ vpcmain.tf
@@ -1,14 +1,12 @@
 provider "aws" {
   region = "us-west-2"
 }
-
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
   tags                 = var.tags
 }
-
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
@@ -16,30 +14,25 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   tags                    = var.tags
 }
-
 resource "aws_subnet" "private" {
   count      = length(var.private_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   tags       = var.tags
 }
-
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
 }
-
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.main.id
   subnet_id     = aws_subnet.public[0].id
   tags          = var.tags
 }
-
 resource "aws_eip" "main" {
   vpc = true
   tags = var.tags
 }
-
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
@@ -48,7 +41,6 @@ resource "aws_route_table" "public" {
   }
   tags = var.tags
 }
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
@@ -57,41 +49,25 @@ resource "aws_route_table" "private" {
   }
   tags = var.tags
 }
-
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
-
 resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
-
-resource "aws_flow_log" "main" {
-  log_destination      = aws_cloudwatch_log_group.main.arn
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.main.id
-  log_destination_type = "cloud-watch-logs"
-}
-
-resource "aws_cloudwatch_log_group" "main" {
-  name = "/aws/vpc/flow-logs"
-}
-
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
-
   ingress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 65535
@@ -99,11 +75,9 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "aws_network_acl" "main" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
-
   ingress {
     protocol   = "tcp"
     rule_no    = 100
@@ -112,7 +86,6 @@ resource "aws_network_acl" "main" {
     from_port  = 0
     to_port    = 65535
   }
-
   egress {
     protocol   = "tcp"
     rule_no    = 100
