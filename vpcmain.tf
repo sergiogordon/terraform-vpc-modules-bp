879332752938fb3ee342887da82123d2b3209928
sergiogordon <13407640+sergiogordon@users.noreply.github.com>
2024-08-07 10:25:45 -0500

    providers outside

--- vpcmain.tf
+++ vpcmain.tf
@@ -1,12 +1,10 @@
-provider "aws" {
-  region = "us-east-1"
-}
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
   tags                 = var.tags
 }
+
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
@@ -14,25 +12,30 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   tags                    = var.tags
 }
+
 resource "aws_subnet" "private" {
   count      = length(var.private_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   tags       = var.tags
 }
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
 }
+
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.main.id
   subnet_id     = aws_subnet.public[0].id
   tags          = var.tags
 }
+
 resource "aws_eip" "main" {
-  vpc = true
+  vpc  = true
   tags = var.tags
 }
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
@@ -41,6 +44,7 @@ resource "aws_route_table" "public" {
   }
   tags = var.tags
 }
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
@@ -49,25 +53,30 @@ resource "aws_route_table" "private" {
   }
   tags = var.tags
 }
+
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
 resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
+
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
+
   ingress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 65535
@@ -75,36 +84,41 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_network_acl" "main" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
+
   ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
+    protocol  = "tcp"
+    rule_no   = 100
+    action    = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 65535
+    from_port = 0
+    to_port   = 65535
   }
+
   egress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
+    protocol  = "tcp"
+    rule_no   = 100
+    action    = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 65535
+    from_port = 0
+    to_port   = 65535
   }
 }
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
+  description = "The ID of the VPC"
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  value       = aws_subnet.private[*].id
+  description = "The IDs of the private subnets"
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  value       = aws_subnet.public[*].id
+  description = "The IDs of the public subnets"
 }
-
